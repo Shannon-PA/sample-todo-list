52d6be3eb753c976b61acf742127dea625b319de
Shannon Buys <shannon.buys@pinnacol.com>
2018-09-04 08:52:13 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  list = [
+    'Get milk',
+    'Play vids',
+    'sleep'
+  ];
+  newToDoItem;
+
+  removeItem(itemToRemove) {
+    this.list = this.list.filter(listItem => listItem !== itemToRemove);
+  }
+
+  addItem(newTodo) {
+    this.list.push(newTodo);
+  }
 }
